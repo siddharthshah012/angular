32f940e1edff81abc6f60d5129875c190f1a4324
siddharthshah012 <sshah8@mail.csuchico.edu>
2017-07-18 08:53:17 -0400

    read authors test

--- untitled/app/resources/business/AdminAuthor/author-constants.js
+++ untitled/app/resources/business/AdminAuthor/author-constants.js
@@ -2,7 +2,7 @@ lmsApp
 		.constant(
 				"authorConstants",
 				{
-					GET_ALL_AUTHORS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllAuthors",
+					GET_ALL_AUTHORS_URL : "http://34.207.112.206:8080/admin/readAllAuthors",
 					GET_AUTHOR_BY_PK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/returnAuthorObjectById/",
 					GET_AUTHOR_INIT_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/getInitAuthor",
 					ADD_AUTHOR_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/addAuthor",
